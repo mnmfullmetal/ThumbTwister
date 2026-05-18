79d02c9f93817b4b5556c4c9868ee45856217dab
myles gillard <mnm.fullmetal@gmail.com>
2026-05-18 21:04:48 +1000

    refactored visualisation to allow for raw and adjusted inputs to be seen
    main.cpp:
    - add ApplyRotation func for rotational matrix math
    
    visualiser.cpp:
    -dynamic screen scalling
    -split layout for thumbsticks
    - circle grid with crosshairs and raw input dots

--- ThumbTwister/main.cpp
+++ ThumbTwister/main.cpp
@@ -1,86 +1,107 @@
-
 #include <iostream>
+#include <cmath>
 #include <GameInput.h>
 #include <windows.h>
 #include <ViGEmClient.h>
 
+#pragma comment(lib, "GameInput.lib")
 #pragma comment(lib, "setupapi.lib")
 #pragma comment(lib, "winmm.lib")
 
 using namespace GameInput::v3;
 
 void StartVisualiser();
-void DrawControllerState(float leftX, float leftY, float rightX, float rightY);
-bool VisualiserShouldClose();
 void StopVisualiser();
+bool VisualiserShouldClose();
+void DrawControllerState(float r_lx, float r_ly, float s_lx, float s_ly, float r_rx, float r_ry, float a_rx, float a_ry);
 
+void ApplyRotation(float raw_x, float raw_y, float offsetDeg, float& out_x, float& out_y)
+{
+    if (raw_x == 0.0f && raw_y == 0.0f) {
+        out_x = 0.0f; out_y = 0.0f;
+        return;
+    }
+
+    float offsetRad = offsetDeg * (3.14159265f / 180.0f);
+    float cosTheta = std::cos(offsetRad);
+    float sinTheta = std::sin(offsetRad);
+
+    out_x = (raw_x * cosTheta) - (raw_y * sinTheta);
+    out_y = (raw_x * sinTheta) + (raw_y * cosTheta);
+
+    if (out_x > 1.0f) out_x = 1.0f; if (out_x < -1.0f) out_x = -1.0f;
+    if (out_y > 1.0f) out_y = 1.0f; if (out_y < -1.0f) out_y = -1.0f;
+}
 
 int main()
 {
-	PVIGEM_CLIENT client = vigem_alloc();
-	PVIGEM_TARGET virtualPad = nullptr;
-	if (client && VIGEM_SUCCESS(vigem_connect(client)))
-	{
-		virtualPad = vigem_target_x360_alloc();
-		vigem_target_add(client, virtualPad);
-	}
-	else
-	{
-		std::cerr << "Failed to initialise ViGEmBus" << std::endl;
-		return -1;
-	}
+    PVIGEM_CLIENT client = vigem_alloc();
+    PVIGEM_TARGET virtualPad = nullptr;
+    if (client && VIGEM_SUCCESS(vigem_connect(client)))
+    {
+        virtualPad = vigem_target_x360_alloc();
+        vigem_target_add(client, virtualPad);
+    }
+    else
+    {
+        std::cerr << "Failed to initialise ViGEmBus" << std::endl;
+        return -1;
+    }
 
-	IGameInput* gameInput = nullptr;
-	if (FAILED(GameInputCreate(&gameInput)))
-	{
-		std::cerr << "Failed to create GameInput instance" << std::endl;
-		return -1;
-	}
-	gameInput->SetFocusPolicy(GameInputEnableBackgroundInput);
+    IGameInput* gameInput = nullptr;
+    if (FAILED(GameInputCreate(&gameInput)))
+    {
+        std::cerr << "Failed to create GameInput instance" << std::endl;
+        return -1;
+    }
+    gameInput->SetFocusPolicy(GameInputEnableBackgroundInput);
 
-	std::cout << "Listening for input)\n\n";
+    std::cout << "Listening for input...\n\n";
 
-	StartVisualiser();
-	while (!VisualiserShouldClose())
-	{
+    StartVisualiser();
+    while (!VisualiserShouldClose())
+    {
+        IGameInputReading* reading = nullptr;
+        GameInputGamepadState state{};
 
-		IGameInputReading* reading = nullptr;
-		GameInputGamepadState state{};
+        if (SUCCEEDED(gameInput->GetCurrentReading(GameInputKindGamepad, nullptr, &reading)))
+        {
+            reading->GetGamepadState(&state);
 
-		if (SUCCEEDED(gameInput->GetCurrentReading(GameInputKindGamepad, nullptr, &reading)))
-		{
-			reading->GetGamepadState(&state);
+            float raw_lx = state.leftThumbstickX;
+            float raw_ly = state.leftThumbstickY;
+            float raw_rx = state.rightThumbstickX;
+            float raw_ry = state.rightThumbstickY;
 
-			XUSB_REPORT report;
-			XUSB_REPORT_INIT(&report);
-			report.sThumbLX = (SHORT)(state.leftThumbstickX * 32767.0f);
-			report.sThumbLY = (SHORT)(state.leftThumbstickY * 32767.0f);
-			report.sThumbRX = (SHORT)(state.rightThumbstickX * 32767.0f);
-			report.sThumbRY = (SHORT)(state.rightThumbstickY * 32767.0f);
+            float adj_lx, adj_ly, adj_rx, adj_ry;
 
-			if (virtualPad) vigem_target_x360_update(client, virtualPad, report);
-			reading->Release();
+            ApplyRotation(raw_lx, raw_ly, 15.0f, adj_lx, adj_ly);
+            ApplyRotation(raw_rx, raw_ry, 15.0f, adj_rx, adj_ry);
 
-			DrawControllerState(state.leftThumbstickX, state.leftThumbstickY, state.rightThumbstickX, state.rightThumbstickY);
-		}
-	}
+            XUSB_REPORT report;
+            XUSB_REPORT_INIT(&report);
+            report.sThumbLX = (SHORT)(state.leftThumbstickX * 32767.0f);
+            report.sThumbLY = (SHORT)(state.leftThumbstickY * 32767.0f);
+            report.sThumbRX = (SHORT)(state.rightThumbstickX * 32767.0f);
+            report.sThumbRY = (SHORT)(state.rightThumbstickY * 32767.0f);
 
-	if (virtualPad)
-	{
-		vigem_target_remove(client, virtualPad);
-		vigem_target_free(virtualPad);
-	}
-	if (client)
-	{
-		vigem_disconnect(client);
-		vigem_free(client);
-	}
-	if (gameInput)
-	{
-		gameInput->Release();
-	}
+            if (virtualPad) vigem_target_x360_update(client, virtualPad, report);
+            reading->Release();
 
-	StopVisualiser();
-	return 0;
+            DrawControllerState(raw_lx, raw_ly, adj_lx, adj_ly, raw_rx, raw_ry, adj_rx, adj_ry);
+        }
+    }
+
+    if (virtualPad) 
+    { 
+        vigem_target_remove(client, virtualPad); vigem_target_free(virtualPad); 
+    }
+    if (client) 
+    { 
+        vigem_disconnect(client); vigem_free(client); 
+    }
+    if (gameInput) gameInput->Release();
+
+    StopVisualiser();
+    return 0;
 }
-
--- ThumbTwister/visualiser.cpp
+++ ThumbTwister/visualiser.cpp
@@ -3,39 +3,70 @@
 #define RAYGUI_IMPLEMENTATION
 #include "raygui.h"
 
-
 void StartVisualiser()
 {
-	SetConfigFlags(FLAG_MSAA_4X_HINT);
-	InitWindow(1200, 800, "ThumbTwister Visualizer");
-	SetTargetFPS(500);
-	SetWindowState(FLAG_WINDOW_RESIZABLE);
+    SetConfigFlags(FLAG_MSAA_4X_HINT);
+    InitWindow(1200, 800, "ThumbTwister Visualizer");
+    SetTargetFPS(500);
+    SetWindowState(FLAG_WINDOW_RESIZABLE);
 
-	GuiSetStyle(DEFAULT, TEXT_SIZE, 20);
-	GuiSetStyle(DEFAULT, TEXT_COLOR_NORMAL, ColorToInt(WHITE));
-	GuiSetStyle(DEFAULT, TEXT_COLOR_FOCUSED, ColorToInt(WHITE));
-	GuiSetStyle(DEFAULT, TEXT_COLOR_PRESSED, ColorToInt(WHITE));
+    GuiSetStyle(DEFAULT, TEXT_SIZE, 20);
+    GuiSetStyle(DEFAULT, TEXT_COLOR_NORMAL, ColorToInt(WHITE));
+    GuiSetStyle(DEFAULT, TEXT_COLOR_FOCUSED, ColorToInt(WHITE));
+    GuiSetStyle(DEFAULT, TEXT_COLOR_PRESSED, ColorToInt(WHITE));
 }
 
-void DrawControllerState(float leftX, float leftY, float rightX, float rightY)
+void DrawControllerState(float rawLeftX, float rawLeftY, float snapLeftX, float snapLeftY, float rawRightX, float rawRightY, float adjRightX, float adjRightY)
 {
-	BeginDrawing();
-	ClearBackground(DARKGRAY);
+    BeginDrawing();
+    ClearBackground(DARKGRAY);
 
-	DrawCircle(150 + (int)(leftX * 50), 150 - (int)(leftY * 50), 20, LIGHTGRAY);
-	DrawText("Left Thumbstick", 100, 200, 10, BLUE);
+    float screenWidth = (float)GetScreenWidth();
+    float screenHeight = (float)GetScreenHeight();
+    float circleRadius = screenHeight * 0.20f;
+    float leftCircleCenterX = screenWidth * 0.25f;
+    float leftCircleCenterY = screenHeight * 0.45f;
+    float rightCircleCenterX = screenWidth * 0.75f;
+    float rightCircleCenterY = screenHeight * 0.45f;
 
-	DrawCircle(350 + (int)(rightX * 50), 150 - (int)(rightY * 50), 20, LIGHTGRAY);
-	DrawText("Right Thumbstick", 300, 200, 10, RED);
-	EndDrawing();
+    float leftBoxX = 40.0f;
+    float leftBoxY = 40.0f;
+    float leftBoxWidth = (screenWidth / 2.0f) - 60.0f;
+    float leftBoxHeight = screenHeight - 80.0f;
+    GuiGroupBox(Rectangle{ leftBoxX, leftBoxY, leftBoxWidth, leftBoxHeight }, "Left Stick");
 
+    DrawCircleLines((int)leftCircleCenterX, (int)leftCircleCenterY, circleRadius, LIGHTGRAY);
+    DrawText("LEFT STICK", (int)(leftCircleCenterX - 60), (int)(leftCircleCenterY - circleRadius - 40), 20, LIGHTGRAY);
+    DrawText(TextFormat("RAW X: %0.4f", rawLeftX), (int)(leftCircleCenterX - circleRadius), (int)(leftCircleCenterY + circleRadius + 20), 20, RED);
+    DrawText(TextFormat("RAW Y: %0.4f", rawLeftY), (int)(leftCircleCenterX - circleRadius), (int)(leftCircleCenterY + circleRadius + 50), 20, RED);
+
+    DrawCircle((int)(leftCircleCenterX + rawLeftX * circleRadius), (int)(leftCircleCenterY - rawLeftY * circleRadius), 14, RED);
+
+    float rightBoxX = (screenWidth / 2.0f) + 20.0f;
+    float rightBoxY = 40.0f;
+    float rightBoxWidth = (screenWidth / 2.0f) - 60.0f;
+    float rightBoxHeight = screenHeight - 80.0f;
+    GuiGroupBox(Rectangle{ rightBoxX, rightBoxY, rightBoxWidth, rightBoxHeight }, "Right Stick");
+
+    DrawCircleLines((int)rightCircleCenterX, (int)rightCircleCenterY, circleRadius, LIGHTGRAY);
+    DrawText("RIGHT STICK", (int)(rightCircleCenterX - 60), (int)(rightCircleCenterY - circleRadius - 40), 20, LIGHTGRAY);
+    DrawText(TextFormat("RAW X: %0.4f", rawRightX), (int)(rightCircleCenterX - circleRadius), (int)(rightCircleCenterY + circleRadius + 20), 20, BLUE);
+    DrawText(TextFormat("RAW Y: %0.4f", rawRightY), (int)(rightCircleCenterX - circleRadius), (int)(rightCircleCenterY + circleRadius + 50), 20, BLUE);
+
+    DrawCircle((int)(rightCircleCenterX + rawRightX * circleRadius), (int)(rightCircleCenterY - rawRightY * circleRadius), 14, BLUE);
+
+    DrawLine((int)(leftCircleCenterX - circleRadius), (int)leftCircleCenterY, (int)(leftCircleCenterX + circleRadius), (int)leftCircleCenterY, Fade(LIGHTGRAY, 0.3f));
+    DrawLine((int)leftCircleCenterX, (int)(leftCircleCenterY - circleRadius), (int)leftCircleCenterX, (int)(leftCircleCenterY + circleRadius), Fade(LIGHTGRAY, 0.3f));
+    DrawLine((int)(rightCircleCenterX - circleRadius), (int)rightCircleCenterY, (int)(rightCircleCenterX + circleRadius), (int)rightCircleCenterY, Fade(LIGHTGRAY, 0.3f));
+    DrawLine((int)rightCircleCenterX, (int)(rightCircleCenterY - circleRadius), (int)rightCircleCenterX, (int)(rightCircleCenterY + circleRadius), Fade(LIGHTGRAY, 0.3f));
+
+    EndDrawing();
 }
 
 void StopVisualiser() {
-	CloseWindow();
+    CloseWindow();
 }
 
-bool VisualiserShouldClose()
-{
-	return WindowShouldClose();
+bool VisualiserShouldClose() {
+    return WindowShouldClose();
 }
